b98474ae6960bb05eeff6e02e651bc9e18ac2a42
Francis Donnelly <Francis.donnelly@baruch.cuny.edu>
2020-01-30 14:06:35 -0500

    Modified to rename tables

--- db_utils/README.md
+++ db_utils/README.md
@@ -4,6 +4,6 @@ These scripts are used for QC and transferring tables from one database to anoth
 
 1. compare_tables: after new data is retrieved, processed, and loaded into a SQLite test database, this script evaluates differences between the new tables and existing tables in the nyc_geodatabase. The following comparisons are made to spot potential errors: equal number of columns and rows, matching and missing identifiers, and the difference between a given data value for the most recent and previous year.
 
-2. sqlite_to_sqlite: once data is inspected and verified, this script is used to copy tables from the test database to what will be the next version of the nyc_gdb, and to drop the tables from the previous year.
+2. sqlite_to_sqlite: once data is inspected and verified, this script is used to copy tables from the test database to what will be the next version of the nyc_gdb, rename the tables, and drop the tables from the previous year.
 
 3. sqlite_to_postgres: the final step in the data acquisition, processing, checking, and publishing process is to archive the data on our local network database. This script copies data from the SQLite nyc_gdb to PostgreSQL.   
--- db_utils/sqlite_to_sqlite.py
+++ db_utils/sqlite_to_sqlite.py
@@ -1,18 +1,21 @@
 #Copy new tables from temporary SQLite database to permanent one and
 #drop old tables from permanent database
-#Recreates tables and inserts records to preserve keys and constraints
+#Recreates and renames tables and inserts records to preserve keys and constraints
 
 import sqlite3, os, sys
 os.chdir('..')
 
 #MODIFY these values to pull from the right sources
 #db1 should be the original db
-db1='nyc_gdb_jan2019.sqlite'
+db1='nyc_gdb_jan2020_DRAFT.sqlite'
 tabdrop=['b_zctas_2016biz_emp','b_zctas_2016biz_ind','b_zctas_2016biz_indcodes']
 
 #db2 should be the test database
+#Keys (k) are the test db table names, values (v) will be the new table names
 db2=os.path.join('census_zbp/outputs/testdb.sqlite')
-tabadd=['zbp2016emp','zbp2016ind','zbp2016indcodes']
+tabadd={'zbp2017emp':'b_zctas_2017biz_emp',
+        'zbp2017ind': 'b_zctas_2017biz_ind',
+        'zbp2017indcodes': 'b_zctas_2017biz_indcodes'}
 
 def table_exists(dbalias,dbname,tablist):
     for t in tablist:
@@ -34,13 +37,13 @@ cur.execute("ATTACH '{}' AS db2;".format(db2))
 
 #Check to make sure tables exist / are spelled correctly in both dbs
 table_exists('main',db1,tabdrop)
-table_exists('db2',db2,tabadd)
+table_exists('db2',db2,tabadd.keys())
 
 print('You are about to drop the following tables from',db1)
 for i in tabdrop:
     print(i) 
 print('\nAnd add these tables from',db2)
-for j in tabadd:
+for j in tabadd.keys():
     print(j) 
 answer = input('Are you sure you want to do this? (y/n): ')
 
@@ -48,18 +51,18 @@ if answer=='y':
     for t in tabdrop:
         cur.execute('DROP TABLE {};'.format(t))
         print('Dropped table',t)
-    for t in tabadd:
-        cur.execute("SELECT sql FROM db2.sqlite_master WHERE type='table' AND name = '{}';".format(t))
-        create = cur.fetchone()[0]
-        cur.execute("SELECT * FROM db2.{};".format(t))
+    for k, v in tabadd.items():
+        cur.execute("SELECT sql FROM db2.sqlite_master WHERE type='table' AND name = '{}';".format(k))
+        create = cur.fetchone()[0].replace(k,v)
+        cur.execute("SELECT * FROM db2.{};".format(k))
         rows=cur.fetchall()
         colcount=len(rows[0])
         pholder='?,'*colcount
         newholder=pholder[:-1]
         cur.execute(create)
-        cur.executemany("INSERT INTO {} VALUES ({});".format(t, newholder),rows)
+        cur.executemany("INSERT INTO {} VALUES ({});".format(v, newholder),rows)
         con.commit() 
-        print('Added table',t)
+        print('Added table',v)
 else:
     print('\nEXITING PROGRAM, no changes made \n')
     con.close()
